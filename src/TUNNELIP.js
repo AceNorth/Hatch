84ece857fea17587c92dab2d0a5613b707ba3e61
Jean Chung <jxchung@gmail.com>
2017-01-31 18:10:59 -0600

    moved facebook login to back of router so it does not interfere

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,3 +1,3 @@
-export const tunnelIP = "https://biymirnnls.localtunnel.me";
+export const tunnelIP = 'https://emwaeuanmv.localtunnel.me';
 
 // type "lt --port 1333" when server is running to update this
