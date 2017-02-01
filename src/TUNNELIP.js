4395d38b263abd64656b96dc72f0fd2335ddc170
Jean Chung <jxchung@gmail.com>
2017-02-01 13:23:45 -0600

    After login, user is routed to landing page

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,4 +1,3 @@
-export const tunnelIP = 'https://qlxzacfyfy.localtunnel.me';
+export const tunnelIP = 'https://pzfxyhiyod.localtunnel.me';
 
 // type "lt --port 1333" when server is running to update this
-
