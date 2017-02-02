50575df092672308d4f5b520d333a9856aab22c2
Jean Chung <jxchung@gmail.com>
2017-02-01 20:13:42 -0600

    removed firebase and switched to pure FB SDK

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,16 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { Scene, Router, Actions } from 'react-native-router-flux';
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
 import LandingPage from './LandingPage';
 import WelcomePage from './WelcomePage';
 import ViewPayload from './ViewPayload';
 import Login from './Login';
 
-
 const RouterComponent = () => (
   <Router>
+    <Scene key="welcomePage" component={WelcomePage} hideNavBar />
     <Scene key="login" component={Login} hideNavBar />
     <Scene key="landingPage" component={LandingPage} title="Left You Somethin" />
     <Scene key="viewPayload" component={ViewPayload} title="Check this out!" />
-    <Scene key="welcomePage" component={WelcomePage} hideNavBar />
   </Router>
 );
 
