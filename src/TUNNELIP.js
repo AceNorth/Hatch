2745decd5117c2e9f3741a74d7a236aff64c01cb
Jean Chung <jxchung@gmail.com>
2017-02-02 16:32:03 -0600

    friends component with profile pictures

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,3 +1,3 @@
-export const tunnelIP = 'https://dwryycbeyj.localtunnel.me';
+export const tunnelIP = 'https://lfmrrqxabx.localtunnel.me';
 
 // type "lt --port 1333" when server is running to update this
