e0d86b12c39c238e8d9ec71c50c633d614ecc092
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-02-01 21:41:27 -0600

    images are working, backend api route set up

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,5 +1,5 @@
 
-export const tunnelIP = 'https://jyxmgdnlkm.localtunnel.me';
+export const tunnelIP = 'https://duyyzwxxdg.localtunnel.me';
 
 // type "lt --port 1333" when server is running to update this
 
