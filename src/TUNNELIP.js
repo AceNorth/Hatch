df6e294f57b8d9c39681d6263f53c024c6f0b0d9
Alexis J <alexis.m.jenn@gmail.com>
2017-02-01 22:27:55 -0600

    pin colors

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,4 +1,4 @@
-export const tunnelIP = "https://nvfwqiljvf.localtunnel.me";
+export const tunnelIP = "https://kkhrvffeiv.localtunnel.me";
 
 
 // type "lt --port 1333" when server is running to update this
