c94e397bf6377c0c19ea636aec8d38d357ad9567
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-02-03 13:27:00 -0600

    Revert "add recipient to AddEgg modal"
    This reverts commit b5ede621b838649ac3427d4900efee55812ed611.

--- src/components/AddEgg.js
+++ src/components/AddEgg.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Modal, TextInput, StyleSheet, Image, Picker} from 'react-native';
+import {Text, View, Modal, TextInput, StyleSheet, Image} from 'react-native';
 import {CardSection} from './common/CardSection';
 import {Button} from './common/Button';
 import {Input} from './common/Input';
@@ -22,8 +22,7 @@ class AddEgg extends Component {
             goHereImageSource: {uri: 'https://facebook.github.io/react/img/logo_og.png'},
             goHereImageBuffer: null,
             eggs: [],
-            recipient: this.props.friends[0].id}
-        ;
+        };
 
         this.handleInputChange=this.handleInputChange.bind(this);
         this.clearInput=this.clearInput.bind(this);
@@ -48,12 +47,11 @@ class AddEgg extends Component {
             payloadImage: this.state.payloadImageSource,
             payloadImageBuffer: this.state.payloadImageBuffer,
             senderId: this.props.senderId,
-            recipient: this.state.recipient
         }
 
         axios.post(`${tunnelIP}/api/egg`, egg)
             .then(()=>{
-                this.setState({ text:'', payloadText: '', goHereText: '', recipient:this.props.friends[0].id});
+                this.setState({ text:'', payloadText: '', goHereText: ''});
                 this.props.showModal(false);
                 this.props.clearAnnotation();
             })
@@ -62,7 +60,7 @@ class AddEgg extends Component {
     }
 
     onCancelSubmitNode() {
-        this.setState({ text:'', payloadText: '', goHereText: '', recipient:this.props.friends[0]});
+        this.setState({ text:'', payloadText: '', goHereText: ''});
         this.props.showModal(false);
         this.props.clearAnnotation();
     }
@@ -132,6 +130,7 @@ class AddEgg extends Component {
 
     render(){
         const { containerStyle, textStyle, cardSectionStyle} = styles;
+
         return (
             <View style={containerStyle}>
                 <CardSection>
@@ -162,19 +161,6 @@ class AddEgg extends Component {
                         onFocus={this.showImagePicker}
                     />
                 </CardSection>
-                <CardSection>
-                    <Picker
-                        style={styles.picker}
-                        selectedValue={this.state.recipient}
-                        onValueChange={(friend) => this.setState({recipient: friend})}>
-                        { this.props.friends.map((friend) => {
-                                return(
-                                    <Picker.Item label={friend.name} value={friend.id} />
-                                )
-                            }
-                        )}
-                    </Picker>
-                </CardSection>
 
                 <CardSection>
                     <Button onPress={this.onSubmitNode}>Submit</Button>
@@ -202,9 +188,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center'
     },
-    picker: {
-        width: 300,
-    },
 });
 
 
@@ -214,8 +197,7 @@ const mapStateToProps = (state, ownProps) => {
     return {
         showAddNodeModal: state.addNodeModal.showAddNodeModal,
         annotation: state.map.annotation,
-        senderId: state.auth.id,
-        friends: state.friends,
+        senderId: state.auth.id
     };
 }
 
