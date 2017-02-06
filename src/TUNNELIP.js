9d17e32e74d2e91cc0534821e6045bb19a831d81
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-02-06 15:18:04 -0600

    images rendering in view payload

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,3 +1,3 @@
-export const tunnelIP = 'https://modcmanmrp.localtunnel.me';
+export const tunnelIP = 'https://pjpwwmtqeb.localtunnel.me';
 
 // type "lt --port 1333" when server is running to update this
--- src/components/ViewPayload.js
+++ src/components/ViewPayload.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { Card, CardSection, Button } from './common';
 import axios from 'axios';
 import { tunnelIP } from '../TUNNELIP';
@@ -12,32 +12,28 @@ class ViewPayload extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewEgg: props.allEggs[props.selectedEgg]
+      viewEgg: props.allEggs[props.selectedEgg],
+        goHereImage:{},
+        payloadImage:{}
     }
   }
 
   componentWillMount(){
     this.setState({viewEgg: this.props.allEggs[this.props.selectedEgg]})
+      let goHereImage2;
+      axios.get(`${tunnelIP}/api/egg/goHereImage/`+ this.props.selectedEgg)
+          .then(response => {
+              goHereImage2 = response.data;
+              this.setState({goHereImage: goHereImage2});
+          })
+      let payloadImage2;
+      axios.get(`${tunnelIP}/api/egg/payloadImage/`+ this.props.allEggs[this.props.selectedEgg].payloadId)
+          .then(response => {
+              payloadImage2 = response.data;
+              this.setState({payloadImage: payloadImage2});
+          })
   }
 
-  componentWillMount(){
-    this.setState({viewEgg: this.props.allEggs[this.props.selectedEgg]})
-  }
-
-  renderPayloadView() {
-    let payloadType = this.state.viewEgg.payload.type;
-    switch (payloadType) {
-      // conditional render for different payloads
-      case 'Text':
-        return (<Text>{ this.state.viewEgg.payload.text }</Text>)
-      case 'Image':
-        //change this to image
-        //render from DB
-        return (<Text>{ this.state.viewEgg.payload.path }</Text>)
-      default:
-        return (<Text>Something has GONE WRONG</Text>)
-    }
-  }
 
   onSubmitPickup(){
     let eggId = this.state.viewEgg.id
@@ -55,15 +51,14 @@ class ViewPayload extends Component {
           <Text style={{fontSize: 30, paddingTop: 50}}> Here's your message! </Text>
         </CardSection>
         <CardSection>
-          { this.renderPayloadView() }
           { this.onSubmitPickup() }
-        </CardSection>
-        <CardSection>
-          <Button
-              color='#517fa4'
-              onPress={Actions.landingPage}
-          >Go Back
-          </Button>
+          <View>
+            <Image style={{width: 50, height: 50}} source={{uri: this.state.goHereImage.uri}}></Image>
+            <Text>{ this.state.viewEgg.goHereText }</Text>
+
+            <Image style={{width: 50, height: 50}} source={{uri: this.state.payloadImage.uri}}></Image>
+            <Text>{ this.state.viewEgg.payload.text }</Text>
+          </View>
         </CardSection>
         <CardSection>
           <Button
@@ -78,8 +73,8 @@ class ViewPayload extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const selectedEgg = state.eggs.selectedEgg;
-  const allEggs = state.eggs.allEggs;
+    const selectedEgg = state.eggs.selectedEgg;
+    const allEggs = state.eggs.allEggs;
     return { selectedEgg, allEggs };
 };
 
