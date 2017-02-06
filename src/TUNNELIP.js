d4bb0d0543be5c37d4cfea64467f22e91dbfcaab
Alexis J <alexis.m.jenn@gmail.com>
2017-02-06 12:41:27 -0600

    toggle/picker to sepeartely show all, sent, and received pins

--- src/TUNNELIP.js
+++ src/TUNNELIP.js
@@ -1,4 +1,4 @@
-export const tunnelIP = 'https://cofqwuuhgj.localtunnel.me';
+export const tunnelIP = 'https://fezmdttpro.localtunnel.me';
 // export const tunnelIP = 'http://172.28.113.203.xip.io:1333';
 
 // type "lt --port 1333" when server is running to update this
